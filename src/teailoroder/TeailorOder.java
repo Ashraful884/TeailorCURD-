c58a6cac918662632bf7997c8ac18d86761ac552
adnan morshed <adnan.starit118@gmail.com>
2020-12-18 05:57:11 -0800

    First commit

---
+++ src/teailoroder/TeailorOder.java
@@ -0,0 +1,12 @@
+
+package teailoroder;
+
+import QueryProcess.Query;
+import java.sql.SQLException;
+
+public class TeailorOder {
+    public static void main(String[] args) throws SQLException {
+     //fggg 
+    }
+    
+}
